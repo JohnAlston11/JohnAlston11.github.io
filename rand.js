47da54085218788345b172c532f00019ef4bb6a0
JohnAlston11 <31191129+JohnAlston11@users.noreply.github.com>
2017-08-26 17:03:44 -0400

    Add files via upload

--- rand.js
+++ rand.js
@@ -4,6 +4,8 @@ var body;
 var choice;
 var aList;
 var clickButton;
+var duoButton;
+var titleButton
 
 var students = [
   "Kevin",
@@ -24,11 +26,22 @@ var students = [
   "Krystal"
 ];
 
+var Titles = [
+    "The Outstanding",
+    "The Hungry",
+    "The Champ",
+    "Excellent",
+    "Deaf",
+    "Web Master",
+    "Mother of Dragons",
+    "Slayer of Lies"
+]
+
 var colors = [
   'hotpink',
   'orange',
   'fuchsia', 0,
-  'lightblue', 0,
+  'yellow', 0,
   'black'
 ];
 var cur = 0;
@@ -55,10 +68,14 @@ document.addEventListener('DOMContentLoaded', () => {
   choice = document.getElementById('choice');
   aList = document.getElementById('aList');
   clickButton = document.getElementById('clickButton');
+  duoButton = document.getElementById('duoButton');
+  titleButton = document.getElementById('titleButton');
 
   pickButton.addEventListener('click', pickOnClick );
   aList.addEventListener('click', aListOnClick );
-  clickButton.addEventListener('click', clickOnPick);    
+  clickButton.addEventListener('click', clickOnPick);
+  duoButton.addEventListener('click', duoOnClick);
+  titleButton.addEventListener('click', titleOnClick)
 
   // build the attendance list
   for (let i=0; i<students.length; i++) {
@@ -72,16 +89,22 @@ document.addEventListener('DOMContentLoaded', () => {
 var pickOnClick = function (event) {
   choice.innerHTML = '&nbsp;'
 
- //   let pickName = chooser();
+  //  let pickName = chooser();
        
     var rand = students.splice(Math.floor(Math.random() * students.length), 1);
+    
     
     
   var x = window.setInterval(() => {
     if (colors[cur] === undefined) {
       window.clearInterval(x);
       cur = 0;
+        if(rand[0].length % 2 === 0){
+            rand[0] = rand[0] + '!';
+            choice.innerText = rand;
+        }else {
       choice.innerText = rand;
+    }
       return;
     }
     if (colors[cur]) header.style.color = colors[cur];
@@ -101,3 +124,35 @@ var clickOnPick = function(event){
     alert("Hey, what's up?");
 }
 
+var duoOnClick = function (event) {
+  choice.innerHTML = '&nbsp;'
+  var rand = students.splice(Math.floor(Math.random() * students.length),1);
+  var rand1 = students.splice(Math.floor(Math.random() * students.length),1);
+  var x = window.setInterval(() => {
+    if (colors[cur] === undefined) {
+      window.clearInterval(x);
+      cur = 0;
+      choice.innerHTML = rand + ' <i class="em em-heart"></i> ' + rand1
+    }
+    if (colors[cur]) header.style.color = colors[cur];
+    if (colors[cur-1]) body.style.backgroundColor = colors[cur-1];
+    cur += 1;
+  }, 200);
+}
+
+
+var titleOnClick = function(event){
+    choice.innerHTML = '&nbsp;'
+    var rand = students.splice(Math.floor(Math.random() * students.length), 1);
+    var rand1 = Titles[Math.floor(Math.random() * Titles.length)]
+    var x = window.setInterval(() => {
+    if (colors[cur] === undefined) {
+      window.clearInterval(x);
+      cur = 0;
+      choice.innerHTML = rand + ' ' + rand1 + ' <i class="em em-punch"></i>'
+    }
+    if (colors[cur]) header.style.color = colors[cur];
+    if (colors[cur-1]) body.style.backgroundColor = colors[cur-1];
+    cur += 1;
+  }, 200);
+}
